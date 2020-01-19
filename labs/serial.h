7da168c84bb5b08b7d39208475d034bf73c5d190
Saksham Dhull <sakshamdhull442@gmail.com>
2020-01-20 00:57:16 +0530

    serial write and is_ready

--- labs/serial.h
+++ labs/serial.h
@@ -6,10 +6,13 @@ namespace serial{
 
   static inline bool is_transmitter_ready(io_t baseport){
     //insert your code here
+    uint8_t lr = io::read8(baseport,5);
+    return ((lr & 32) == 32);
   }
 
   static inline void writechar(uint8_t c, io_t baseport){
     //insert your code here
+    io::write8(baseport, 0, c);
   }
 
 } //end serial
