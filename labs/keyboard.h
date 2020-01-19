34a84e64eb557e9a3665c876840a1e6f6af4a370
Saksham Dhull <sakshamdhull442@gmail.com>
2020-01-20 01:23:47 +0530

    abstract mmio/pmio

--- labs/keyboard.h
+++ labs/keyboard.h
@@ -5,14 +5,14 @@
 namespace lpc_kbd{
 
   static inline bool has_key(lpc_kbd_t& dev){
+    return (lpc_kbd_status_obf_extract(lpc_kbd_status_rd(&dev)) == 1);
 
     //insert your code here
   }
 
   static inline uint8_t get_key(lpc_kbd_t& dev){
-
+    return lpc_kbd_input_rd(&dev);
     //insert your code here
   }
 
 }// namespace lpc_kbd
-
