5c97e2d3c343bc3f7d921ac6e1fcb6a6851d903f
Ananye Agarwal <ananayagarwal@gmail.com>
2020-01-22 20:59:38 +0530

    Add color codes to the vgatext.h

--- labs/vgatext.h
+++ labs/vgatext.h
@@ -1,5 +1,15 @@
 #include "util/io.h"
 
+// Color codes ============
+// 0x0 - Black
+// 0x1 - Blue
+// 0x2 - Green
+// 0x3 - Cyan
+// 0x4 - Red
+// 0x5 - Magenta
+// 0x6 - Brown
+// 0x7 - White
+
 namespace vgatext{
 
    static inline void writechar(int loc, uint8_t c, uint8_t bg, uint8_t fg, addr_t base){
